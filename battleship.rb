21d1fa57722efe2b0e72ed65d9a10b6e21fafa23
markhilkert <mark.hilkert@gmail.com>
2019-03-13 19:34:15 -0500

    fixed battleship class initialize

--- battleship.rb
+++ battleship.rb
@@ -1,8 +1,8 @@
 class Battleship
-  def initialize
-    @length = nil
-    @sunk? = false
-    @placed? = false
+  def initialize(length)
+    @length = length
+    @sunk = false
+    @placed = false
     @orientation = 0
     @x_coordinate = 0
     @y_coordinate = 0
---
+++ player.rb
@@ -0,0 +1,11 @@
+class Player
+  attr_accessor :unplaced_ships
+
+  def initialize
+    @unplaced_ships = [Battleship.new(5), Battleship.new(4), Battleship.new(3), Battleship.new(3), Battleship.new(2)]
+  end
+end
+
+player = Player.new
+p player.unplaced_ships
+
